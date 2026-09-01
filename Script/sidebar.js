c0239b9539506a84ec3a4d495fa1ec75988f6445
Kanomwan <kanomwanpiyaman@gmail.com>
2026-09-01 21:49:49 +0700

    1.9.26 2

--- Script/sidebar.js
+++ Script/sidebar.js
@@ -8,7 +8,7 @@ let navbar_code_str = `
   </div>
   <div class="w3-bar-block">
     <a href="index.html" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Home</a> 
-    <a href="#services" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Character</a> 
+    <a href="TOU.html" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Character</a> 
     <a href="UST.html" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">UST</a> 
     <a href="Contact.html" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Contact</a>
   </div>
