d992166ed72a36a59c5a0f71e97c13c0eb9381eb
Kanomwan <kanomwanpiyaman@gmail.com>
2026-09-05 16:17:37 +0700

    .

--- Script/Kanomwan.js
+++ Script/Kanomwan.js
@@ -18,7 +18,7 @@ strong ?<br>
 D#4`,
         "link":"https://www.mediafire.com/download/nbzh4qwg9agr1y6/kanomwan_CV.rar",
         "type":"video",
-        "source":"https://www.youtube.com/embed/WS1dSqD9aqQ?wmode=opaque"
+        "source":"https://www.youtube.com/watch?v=WS1dSqD9aqQ"
     },
         {
         "info":`<b>ACT 3</b><br>
